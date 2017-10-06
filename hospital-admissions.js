6e9134d40266e510caa0c31950dfb8413c1e7924
Amanuel <yihdego@msn.com>
2017-10-06 15:43:00 -0700

    able to display all admission records

--- hospital-admissions.js
+++ hospital-admissions.js
@@ -1,15 +1,28 @@
+const admissions = [];
 class Admission {
-  constructor(patientName, attendingPhysician, admitTime, dischargeTime = null){
+  constructor(patientName, attendingPhysician, admitTime, dischargeTime = "" ){
     this.patientName = patientName;
     this.attendingPhysician = attendingPhysician;
     this.admitTime = admitTime;
     this.dischargeTime = dischargeTime;
+    admissions.push(this);
   }
 }
 
 const amanuel = new Admission('Amanuel Yihdego', 'Elesa Mengesha',"2016-01-01 16:45");
 
-console.log(amanuel.patientName);
-console.log(amanuel.attendingPhysician);
-console.log(amanuel.admitTime);
-console.log(amanuel.dischargeTime);
+const taylor = new Admission('Taylor Whathisname', 'Dr. Suess', "friday the 13ths", "thursday the 12th")
+
+
+const displayAdmissions = () => {
+  let i = 0;
+  const log = admissions.length;
+  console.log(
+    "Patient Name         Attending     Admit Time      Discharge Time Physician\n===========================================================================\n")
+  while (i < log) {
+    console.log(admissions[i].patientName + '    ' + admissions[i].attendingPhysician + '    ' + admissions[i].admitTime + '    ' + admissions[i].dischargeTime);
+    i += 1;
+  }
+}
+
+displayAdmissions();
