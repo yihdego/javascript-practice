a8811c3ac8bfc44572aba7bbf0a58d46be875ed0
Amanuel <yihdego@msn.com>
2017-10-06 16:35:21 -0700

    filtering method for admission records

--- hospital-admissions.js
+++ hospital-admissions.js
@@ -13,8 +13,9 @@ const amanuel = new Admission('Amanuel Yihdego', 'Elesa Mengesha',"2016-01-01 16
 
 const taylor = new Admission('Taylor Whathisname', 'Dr. Suess', "friday the 13ths", "thursday the 12th")
 
+const mcdonald = new Admission('Old McDonald', 'Elesa Mengesha', "April Fools", "April 4th")
 
-const displayAdmissions = () => {
+const displayAllAdmissions = () => {
   let i = 0;
   const log = admissions.length;
   console.log(
@@ -25,4 +26,31 @@ const displayAdmissions = () => {
   }
 }
 
-displayAdmissions();
+displayAllAdmissions();
+
+const displaySomeAdmissions = (some) => {
+  let i = 0;
+  const log = some.length;
+  console.log(
+    "Patient Name         Attending     Admit Time      Discharge Time Physician\n===========================================================================\n")
+  while (i < log) {
+    console.log(some[i].patientName + '    ' + some[i].attendingPhysician + '    ' + some[i].admitTime + '    ' + some[i].dischargeTime);
+    i += 1;
+  }
+}
+
+const findAttribute = (attribute, value) => {
+  let i = 0;
+  const some = [];
+  const log = admissions.length;
+  while (i < log ) {
+    if (admissions[i][attribute] === value) {
+      some.push(admissions[i])
+    }
+      i += 1;
+  }
+  displaySomeAdmissions(some);
+}
+
+findAttribute('attendingPhysician', "Elesa Mengesha")
+findAttribute('patientName', 'Amanuel Yihdego')
