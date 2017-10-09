f60807d668ebf7d1758a1539134e73613f1201ba
Amanuel <yihdego@msn.com>
2017-10-08 18:40:59 -0700

    made 2 jquery event listeners but not able to tie it together because html is weird

--- application.js
+++ application.js
@@ -1,3 +1,88 @@
-$(document).ready(() => {
+class Person {
+  constructor(name){
+    this.name = name;
+  }
+}
 
+class Employee extends Person {
+  constructor(name, position){
+    super(name)
+    this.position = position;
+  }
+}
+
+const admissions = [];
+class Admission {
+  constructor(patientName, attendingPhysician, admitTime, dischargeTime = "" ){
+    this.patientName = patientName;
+    this.attendingPhysician = attendingPhysician;
+    this.admitTime = admitTime;
+    this.dischargeTime = dischargeTime;
+    admissions.push(this);
+  }
+}
+
+const amanuel = new Admission('Amanuel Yihdego', 'Elesa Mengesha',"2016-01-01 16:45");
+
+const taylor = new Admission('Taylor Whathisname', 'Dr. Suess', "friday the 13ths", "thursday the 12th")
+
+const mcdonald = new Admission('Old McDonald', 'Elesa Mengesha', "April Fools", "April 4th")
+
+const displayAllAdmissions = () => {
+  const display = []
+  // if (user['position']==='Doctor' || user['position']==="Nurse") {
+    let i = 0;
+    const log = admissions.length;
+      while (i < log) {
+      display.push(admissions[i].patientName + '    ' + admissions[i].attendingPhysician + '    ' + admissions[i].admitTime + '    ' + admissions[i].dischargeTime);
+        i += 1;
+    // }
+    // unable to use else case GET HELP
+    // else {
+    //   console.log('you shall not pass')
+    // }
+  }
+  return display
+}
+
+const amanuelyihdego = new Person('Amanuel Yihdego')
+const negistiegiday = new Employee('Negistie Teklue', 'Janitor')
+const elesamengesha = new Employee('Elesa Mengesha', 'Doctor')
+
+
+const displaySomeAdmissions = (some) => {
+  let i = 0;
+  const log = some.length;
+  console.log(
+    "Patient Name         Attending     Admit Time      Discharge Time Physician\n===========================================================================\n")
+  while (i < log) {
+    console.log(some[i].patientName + '    ' + some[i].attendingPhysician + '    ' + some[i].admitTime + '    ' + some[i].dischargeTime);
+    i += 1;
+  }
+}
+
+const findAttribute = (attribute, value) => {
+  let i = 0;
+  const some = [];
+  const log = admissions.length;
+  while (i < log ) {
+    if (admissions[i][attribute] === value) {
+      some.push(admissions[i])
+    }
+      i += 1;
+  }
+  displaySomeAdmissions(some);
+}
+$(document).ready(() => {
+    $('.login-form').on('submit', (event) => {
+    const $submitted = event.currentTarget
+    const name = $('.login-form').find('input[name]').val();
+    console.log(name)
+    alert('hello')
+    debugger
+  })
+
+  $('.displayAll').on('click', (event) => {
+    $('.displaying-admissions').append(displayAllAdmissions())
+  })
 });
--- hospital-admissions.js
+++ hospital-admissions.js
@@ -1,80 +1,80 @@
-class Person {
-  constructor(name){
-    this.name = name;
-  }
-}
-
-class Employee extends Person {
-  constructor(name, position){
-    super(name)
-    this.position = position;
-  }
-}
-
-const admissions = [];
-class Admission {
-  constructor(patientName, attendingPhysician, admitTime, dischargeTime = "" ){
-    this.patientName = patientName;
-    this.attendingPhysician = attendingPhysician;
-    this.admitTime = admitTime;
-    this.dischargeTime = dischargeTime;
-    admissions.push(this);
-  }
-}
-
-const amanuel = new Admission('Amanuel Yihdego', 'Elesa Mengesha',"2016-01-01 16:45");
-
-const taylor = new Admission('Taylor Whathisname', 'Dr. Suess', "friday the 13ths", "thursday the 12th")
-
-const mcdonald = new Admission('Old McDonald', 'Elesa Mengesha', "April Fools", "April 4th")
-
-const displayAllAdmissions = (user) => {
-  if (user['position']==='Doctor' || user['position']==="Nurse") {
-    let i = 0;
-    const log = admissions.length;
-    console.log(
-      "Dipslaying all admissions for " + user['name'] + "\nPatient Name         Attending     Admit Time      Discharge Time Physician\n===========================================================================\n")
-    while (i < log) {
-      console.log(admissions[i].patientName + '    ' + admissions[i].attendingPhysician + '    ' + admissions[i].admitTime + '    ' + admissions[i].dischargeTime);
-      i += 1;
-    }
-    // unable to use else case GET HELP
-    // else {
-    //   console.log('you shall not pass')
-    // }
-  }
-}
-
-const amanuelyihdego = new Person('Amanuel Yihdego')
-const negistiegiday = new Employee('Negistie Teklue', 'Janitor')
-const elesamengesha = new Employee('Elesa Mengesha', 'Doctor')
-
-displayAllAdmissions(amanuelyihdego);
-displayAllAdmissions(elesamengesha);
-
-const displaySomeAdmissions = (some) => {
-  let i = 0;
-  const log = some.length;
-  console.log(
-    "Patient Name         Attending     Admit Time      Discharge Time Physician\n===========================================================================\n")
-  while (i < log) {
-    console.log(some[i].patientName + '    ' + some[i].attendingPhysician + '    ' + some[i].admitTime + '    ' + some[i].dischargeTime);
-    i += 1;
-  }
-}
-
-const findAttribute = (attribute, value) => {
-  let i = 0;
-  const some = [];
-  const log = admissions.length;
-  while (i < log ) {
-    if (admissions[i][attribute] === value) {
-      some.push(admissions[i])
-    }
-      i += 1;
-  }
-  displaySomeAdmissions(some);
-}
-
-findAttribute('attendingPhysician', "Elesa Mengesha")
-findAttribute('patientName', 'Amanuel Yihdego')
+// class Person {
+//   constructor(name){
+//     this.name = name;
+//   }
+// }
+//
+// class Employee extends Person {
+//   constructor(name, position){
+//     super(name)
+//     this.position = position;
+//   }
+// }
+//
+// const admissions = [];
+// class Admission {
+//   constructor(patientName, attendingPhysician, admitTime, dischargeTime = "" ){
+//     this.patientName = patientName;
+//     this.attendingPhysician = attendingPhysician;
+//     this.admitTime = admitTime;
+//     this.dischargeTime = dischargeTime;
+//     admissions.push(this);
+//   }
+// }
+//
+// const amanuel = new Admission('Amanuel Yihdego', 'Elesa Mengesha',"2016-01-01 16:45");
+//
+// const taylor = new Admission('Taylor Whathisname', 'Dr. Suess', "friday the 13ths", "thursday the 12th")
+//
+// const mcdonald = new Admission('Old McDonald', 'Elesa Mengesha', "April Fools", "April 4th")
+//
+// const displayAllAdmissions = (user) => {
+//   if (user['position']==='Doctor' || user['position']==="Nurse") {
+//     let i = 0;
+//     const log = admissions.length;
+//     console.log(
+//       "Dipslaying all admissions for " + user['name'] + "\nPatient Name         Attending     Admit Time      Discharge Time Physician\n===========================================================================\n")
+//     while (i < log) {
+//       console.log(admissions[i].patientName + '    ' + admissions[i].attendingPhysician + '    ' + admissions[i].admitTime + '    ' + admissions[i].dischargeTime);
+//       i += 1;
+//     }
+//     // unable to use else case GET HELP
+//     // else {
+//     //   console.log('you shall not pass')
+//     // }
+//   }
+// }
+//
+// const amanuelyihdego = new Person('Amanuel Yihdego')
+// const negistiegiday = new Employee('Negistie Teklue', 'Janitor')
+// const elesamengesha = new Employee('Elesa Mengesha', 'Doctor')
+//
+//
+// const displaySomeAdmissions = (some) => {
+//   let i = 0;
+//   const log = some.length;
+//   console.log(
+//     "Patient Name         Attending     Admit Time      Discharge Time Physician\n===========================================================================\n")
+//   while (i < log) {
+//     console.log(some[i].patientName + '    ' + some[i].attendingPhysician + '    ' + some[i].admitTime + '    ' + some[i].dischargeTime);
+//     i += 1;
+//   }
+// }
+//
+// const findAttribute = (attribute, value) => {
+//   let i = 0;
+//   const some = [];
+//   const log = admissions.length;
+//   while (i < log ) {
+//     if (admissions[i][attribute] === value) {
+//       some.push(admissions[i])
+//     }
+//       i += 1;
+//   }
+//   displaySomeAdmissions(some);
+// }
+// 
+// displayAllAdmissions(amanuelyihdego);
+// displayAllAdmissions(elesamengesha);
+// findAttribute('attendingPhysician', "Elesa Mengesha")
+// findAttribute('patientName', 'Amanuel Yihdego')
